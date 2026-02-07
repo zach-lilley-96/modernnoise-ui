226fa4336f44f9443a31075b326bc619f4d21b8a
zach lilley <zlilley96@gmail.com>
2026-02-07 16:55:19 -0500

    Correct headers to include application/json

--- src/helpers/addNewFriend.ts
+++ src/helpers/addNewFriend.ts
@@ -1,6 +1,9 @@
 export default async function addNewFriend(friendCode: string){
     const response = await fetch(`${import.meta.env.VITE_API_BASE_URI}friends`, {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify({friendCode}),
         credentials: "include"
     })
@@ -8,5 +11,5 @@ export default async function addNewFriend(friendCode: string){
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
-    return response.json();
+    return ;
 }
