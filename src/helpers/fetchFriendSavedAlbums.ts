6ab6c21d699e5ed799510e21c463fa58cc2bddec
zach lilley <zlilley96@gmail.com>
2026-02-08 14:13:22 -0500

    Inline ratings return

--- src/helpers/fetchFriendSavedAlbums.ts
+++ src/helpers/fetchFriendSavedAlbums.ts
@@ -3,10 +3,12 @@ import type {SavedRatingsDto} from "../types/SavedRatingsDto.ts";
 export default async function fetchFriendSavedAlbums(friendCode: string, artistId: string): Promise<SavedRatingsDto[]> {
     const response = await fetch(`${import.meta.env.VITE_API_BASE_URI}rating/friend-ratings/${artistId}?friendCode=${friendCode}`, {
         credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        }
     });
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
-    const ratings: SavedRatingsDto[] = await response.json();
-    return ratings;
+    return await response.json() as SavedRatingsDto[];
 }
